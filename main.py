cfdc15ea1f42a01ff92f306a593cf1a71517c81a
Abdullah Shaikh <121804317+AbdullahSK03@users.noreply.github.com>
2023-08-03 17:52:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
-from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
-# from flask import requests as REQ
-# import smtplib
+from flask import Flask, render_template, req
+# from flask_sqlalchemy import SQLAlchemy
+from flask import requests as REQ
+import smtplib
 
 
 # api = REQ.get("https://api.quotable.io/random").json()
 app = Flask(__name__)
+connection = smtplib.SMTP('smtp.gmail.com')
+connection.starttls()
+with connection.login('abdhuzyfa@gmail.com', 'zaupklzasocwfvgs') as smtp:
+    smtp.sendmail('abdhuzyfa@gmail.com',f'{REQ.form["email"]} {REQ.form["message"]}', msg = f'from: {REQ.form["email"]}\nto: {REQ.form["email"]}\nsubject: {REQ.form["subject"]}\n\n{REQ.form["message"]}')
 
 
 
